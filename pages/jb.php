281f200a23da60034349987fde29d32e7efe16c8
belalstore12 <130758757+belalstore12@users.noreply.github.com>
2023-05-09 22:09:53 +0300

    Add files via upload

---
+++ pages/jb.php
@@ -0,0 +1,44 @@
+<div class="page">
+  
+  
+  
+                    <div class="navbar">
+                        <div class="navbar-inner sliding">
+                                  <div class="left">
+          <a href="#" class="link back"><span class=
+          "f7-icons">arrow_left</span>Back</a>
+        </div>  
+                                <div class="right">
+                                <a href="javascript:window.location.reload(true)" class="f7-icons rotate">reload</a>
+                            </div>
+                            <div class="subnavbar">
+                                <form data-search-in=".item-title" data-search-container=".list" class="searchbar searchbar-init">
+                                    <div class="searchbar-inner">
+                                        <div class="searchbar-input-wrap">
+                                            <input type="search" placeholder="Search">
+                                            <i class="searchbar-icon"></i>
+                                            <span class="input-clear-button"></span>
+                                        </div>
+                                        <span class="searchbar-disable-button if-not-aurora">Cancel</span>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="page-content">
+                        <div class="searchbar-backdrop"></div>
+                        <div class="elevation-12 list media-list inset searchbar-found">
+                            <ul>
+ 
+                              
+                                                                                 
+
+                                        </a>
+                                    </li>
+                             
+                               
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
---
+++ pages/server.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Server</title>
+</head>
+<body>
+  <div class="page">
+    <div class="navbar">
+      <div class="navbar-inner sliding">
+        <div class="left">
+ <a href="#" class="link back">
+          <i class="white-color f7-icons">arrow_left</i> Back  </a>
+        </div>
+        <div class="title">
+          Documentation Of Which Apps Are Available ?
+        </div>
+        <div class="right">
+          <a href="javascript:window.location.reload(true)" class=
+          "f7-icons">reload</a>
+        </div>
+      </div>
+    </div>
+    <div class="page-content">
+      <div class="list media-list inset elevation-12">
+        <ul>
+          <br>
+        <center>
+     Video Star PRO🟢
+          <br>
+          Pubg Mobile 🟢
+          <br>
+          Gta : San Andreas🟢
+          <br>
+          Minecraft🟢
+          
+         </center>
+         </br>     
+        </ul>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
